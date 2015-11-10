8cefad3ae53ac190e5f7d0f754c40d0eaa8a491c
Anton Puko <stinger_anton@mail.ru>
2015-11-10 18:20:25 +0200

    -some small cahnges on bb vs sb 3 max ranges -change RWindow results from VPP to rubs

--- pAd/RWindow.xaml.cs
+++ pAd/RWindow.xaml.cs
@@ -1,24 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
 using System.Diagnostics;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using Newtonsoft.Json.Linq;
-using Npgsql;
 
 namespace Pingvi
 {
@@ -133,8 +118,8 @@ namespace Pingvi
 
 
             const double vppMultiplicator = 5.5;
-           // const double bonusFormula = 3.5/40000*600;
-            rakeback = rake*vppMultiplicator;// * bonusFormula * _usdRubExRate;
+            const double bonusFormula = 3.5/40000*600;
+            rakeback = rake*vppMultiplicator * bonusFormula * _usdRubExRate;
 
             
             
